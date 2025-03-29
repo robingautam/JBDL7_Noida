3afd14f5280d9131c7f1a6a4566f397ef5806e78
Robin <31219114+robingautam@users.noreply.github.com>
2025-03-29 16:59:12 +0530

    Add files via upload

---
+++ JBDL7_Foundation/src/Main.java
@@ -0,0 +1,22 @@
+import model.Student;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 19;
+        //System.out.println(a);
+       // System.out.println(customMethod(2,3));
+        //String name = "Sagar";
+       // System.out.println(name.charAt(2));
+
+        Student student = new Student();
+        File f = new File("");
+    }
+
+    public static int customMethod(int a, int b){
+        return a+b;
+    }
+
+
+}
---
+++ JBDL7_Foundation/src/p4/PersonMain.java
@@ -0,0 +1,11 @@
+package p4;
+
+import p1.p2.Person;
+
+public class PersonMain {
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        Test t = new Test();
+    }
+}
